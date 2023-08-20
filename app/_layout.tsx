06ea6946602131510b6c1bb3be92db8f8d9d2cc0
Emmanuel Ademuyiwa <techienelson@gmail.com>
2023-08-20 11:47:12 +0100

    App initiation with ativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import '../global.css';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -12,7 +13,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: 'welcome',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -48,8 +49,8 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name='index' options={{ headerShown: false }} />
+        <Stack.Screen name='welcome' options={{ headerShown: false }} />
       </Stack>
     </ThemeProvider>
   );
---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'expo-router';
+import type { FC } from 'react';
+import { Pressable, SafeAreaView, Text, View } from 'react-native';
+
+interface indexProps {}
+
+const index: FC<indexProps> = ({}) => {
+  return (
+    <SafeAreaView className='flex-1'>
+      <View className='flex-1 items-center justify-center'>
+        <Text>index</Text>
+      <Link href={'/modal'} asChild>
+        <Text>Go to the next page</Text>
+      </Link>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default index;
